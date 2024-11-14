5cdb1efbc7871298b8439c94e2adefdf1d7e109d
Greider28 <73250074+Greider28@users.noreply.github.com>
2024-11-14 20:52:36 +0200

    Update Solution.java

--- src/com/javarush/test/level04/lesson10/task04/Solution.java
+++ src/com/javarush/test/level04/lesson10/task04/Solution.java
@@ -1,1 +1,44 @@
+package com.javarush.task.task04.task0433;
 
+/* 
+Гадание на долларовый счет
+Вывести на экран квадрат из 10х10 букв S используя цикл while. Буквы в каждой строке не разделять.
+Пример вывода на экран: SSSSSSSSSS
+SSSSSSSSSS
+SSSSSSSSSS
+SSSSSSSSSS
+SSSSSSSSSS
+SSSSSSSSSS
+SSSSSSSSSS
+SSSSSSSSSS
+SSSSSSSSSS
+SSSSSSSSSS
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        int a = 0;
+        while (a < 10){
+            if (a == 9){
+                int i = 0;
+                while (i < 10) {
+                    System.out.print("S");
+                    i++;
+                }
+                a++;
+            }
+            else {
+                int i = 0;
+                while (i < 10) {
+                    System.out.print("S");
+                    i++;
+                }
+                System.out.println();
+                a++;
+            }
+        }
+
+
+    }
+}
