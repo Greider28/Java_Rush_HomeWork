3486afbbf5b245c278436602c0a026801f77c984
Greider28 <73250074+Greider28@users.noreply.github.com>
2024-11-14 20:54:36 +0200

    Update Solution.java

--- src/com/javarush/test/level04/lesson10/task03/Solution.java
+++ src/com/javarush/test/level04/lesson10/task03/Solution.java
@@ -1,1 +1,25 @@
+package com.javarush.task.task04.task0432;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+Хорошего много не бывает
+Ввести с клавиатуры строку и число N больше 0. Вывести на экран строку N раз используя цикл while. Каждое значение с новой строки.
+Пример ввода: абв 2
+Пример вывода: абв абв
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String s = reader.readLine();
+        int n = Integer.parseInt(reader.readLine());
+        while (n > 0){
+            System.out.println(s);
+            n--;
+        }
+
+    }
+}
