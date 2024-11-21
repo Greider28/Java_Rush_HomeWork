8229d453b409a5388b03a116ec43a57abc7c9c27
Greider28 <73250074+Greider28@users.noreply.github.com>
2024-11-21 23:17:47 +0200

    Update Solution.java

--- src/com/javarush/test/level04/lesson13/task04/Solution.java
+++ src/com/javarush/test/level04/lesson13/task04/Solution.java
@@ -1,1 +1,21 @@
+package com.javarush.task.task04.task0438;
 
+/* 
+Рисуем линии
+Используя цикл for вывести на экран: - горизонтальную линию из 10 восьмёрок - вертикальную линию из 10 восьмёрок (символ из горизонтальной строки не учитывается).
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+      for (int i = 0; i < 10; i++) {
+            System.out.print(8);
+        }
+        System.out.println();
+        for (int i = 0; i < 10; i++) {
+            System.out.println(8);
+
+        }
+
+    }
+}
