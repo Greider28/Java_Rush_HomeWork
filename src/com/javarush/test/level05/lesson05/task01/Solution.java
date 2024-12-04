168904427a6633da5fc9fcfdd199041960a694ba
Greider28 <73250074+Greider28@users.noreply.github.com>
2024-12-04 21:20:41 +0200

    Update Solution.java

--- src/com/javarush/test/level05/lesson05/task01/Solution.java
+++ src/com/javarush/test/level05/lesson05/task01/Solution.java
@@ -1,1 +1,16 @@
+package com.javarush.task.task05.task0501;
 
+/* 
+Создание кота
+Создать класс Cat. У кота должно быть имя (name, String), возраст (age, int), вес (weight, int), сила (strength, int).
+*/
+
+public class Cat {
+    //напишите тут ваш код
+  String name;
+    int age, weight, strength;
+
+    public static void main(String[] args) {
+
+    }
+}
