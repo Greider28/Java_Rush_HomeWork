9a7261dc0d3b4cf5824e38de7681032133f98270
Greider28 <73250074+Greider28@users.noreply.github.com>
2024-12-10 20:24:46 +0200

    Update Solution.java

--- src/com/javarush/test/level05/lesson09/task07/Solution.java
+++ src/com/javarush/test/level05/lesson09/task07/Solution.java
@@ -12,6 +12,20 @@ public class Circle {
     public double radius;
 
     //напишите тут ваш код
+    public Circle(double x, double y, double radius){
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+    };
+    public Circle(double x, double y){
+        this(x, y, 10);
+    };
+    public Circle(double radius){
+        this(25, 50, radius);
+    };
+    public Circle(){
+        this(5,7,9);
+    };
 
     public static void main(String[] args) {
 
