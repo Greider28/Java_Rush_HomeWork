f963d72b722fbdbd0de2931d9a921cea1fa0a74e
Greider28 <73250074+Greider28@users.noreply.github.com>
2024-12-10 20:15:46 +0200

    Update Solution.java

--- src/com/javarush/test/level05/lesson09/task07/Solution.java
+++ src/com/javarush/test/level05/lesson09/task07/Solution.java
@@ -1,1 +1,19 @@
+package com.javarush.task.task05.task0522;
 
+/* 
+Максимум конструкторов
+Изучи класс Circle. Напиши максимальное количество конструкторов с разными аргументами.
+Подсказка: не забудь про конструктор по умолчанию.
+*/
+
+public class Circle {
+    public double x;
+    public double y;
+    public double radius;
+
+    //напишите тут ваш код
+
+    public static void main(String[] args) {
+
+    }
+}
