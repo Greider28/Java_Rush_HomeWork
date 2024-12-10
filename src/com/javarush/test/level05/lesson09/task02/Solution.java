50b6c0e5971a04f739f616bf8361cb0528b72a57
Greider28 <73250074+Greider28@users.noreply.github.com>
2024-12-10 19:34:09 +0200

    Update Solution.java

--- src/com/javarush/test/level05/lesson09/task02/Solution.java
+++ src/com/javarush/test/level05/lesson09/task02/Solution.java
@@ -1,1 +1,57 @@
+package com.javarush.task.task05.task0517;
 
+/* 
+Конструируем котиков
+Создать класс Cat (кот) с пятью конструкторами:
+- Имя,
+- Имя, вес, возраст
+- Имя, возраст (вес стандартный)
+- вес, цвет, (имя, адрес и возраст - неизвестные. Кот - бездомный)
+- вес, цвет, адрес (чужой домашний кот)
+Задача конструктора - сделать объект валидным.
+Например, если вес не известен, то нужно указать какой-нибудь средний вес.
+Кот не может ничего не весить. То же касательно возраста. А вот имени может и не быть (null). То же касается адреса: null.
+*/
+
+public class Cat {
+    //напишите тут ваш код
+   String name;
+    int age;
+    int weight;
+    String address;
+    String color;
+
+    public Cat(String name){
+        this.name = name;
+        this.age = 1;
+        this.weight = 1;
+        this.color = "grey";
+    };
+    public Cat(String name, int weight, int age){
+        this.name = name;
+        this.weight = weight;
+        this.age = age;
+        this.color = "grey";
+    };
+    public Cat(String name, int age){
+        this.name = name;
+        this.weight = 1;
+        this.age = age;
+        this.color = "grey";
+    };
+    public Cat(int weight, String color){
+        this.weight = weight;
+        this.color = color;
+        this.age = 1;
+    };
+    public Cat(int weight, String color, String address){
+        this.weight = weight;
+        this.color = color;
+        this.address = address;
+        this.age = 1;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
