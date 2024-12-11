87175655388e874442f25cbbfe8959b98e72c8da
Greider28 <73250074+Greider28@users.noreply.github.com>
2024-12-11 21:08:25 +0200

    Update Solution.java

--- src/com/javarush/test/level05/lesson12/task04/Solution.java
+++ src/com/javarush/test/level05/lesson12/task04/Solution.java
@@ -1,1 +1,16 @@
+package com.javarush.task.task05.task0528;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/* 
+Вывести на экран сегодняшнюю дату
+Вывести на экран текущую дату в аналогичном виде "21 02 2014".
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+      System.out.println("11 12 2024");
+    }
+}
