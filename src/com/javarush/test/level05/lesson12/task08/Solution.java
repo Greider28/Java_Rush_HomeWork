04d296f2569c2a53d485ae218b542e58f68d068b
Greider28 <73250074+Greider28@users.noreply.github.com>
2024-12-11 21:58:53 +0200

    Update Solution.java

--- src/com/javarush/test/level05/lesson12/task08/Solution.java
+++ src/com/javarush/test/level05/lesson12/task08/Solution.java
@@ -1,1 +1,31 @@
+package com.javarush.task.task05.task0532;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+Задача по алгоритмам Ӏ Java Syntax: 5 уровень, 12 лекция
+Написать программу, которая: 
+1. считывает с консоли число N, которое должно быть больше 0 
+2. потом считывает N чисел с консоли 3. выводит на экран максимальное из введенных N чисел.
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        //int maximum = ;//remove line, it will be later
+        int n = Integer.parseInt(reader.readLine());//but add this line for N
+
+        //напишите тут ваш код
+      if (n > 0) {
+            int maximum = Integer.parseInt(reader.readLine());
+            for (int i = 1; i < n; i++){
+                int enterNum = Integer.parseInt(reader.readLine());
+                maximum = (maximum >= enterNum) ? maximum : enterNum;
+            }
+            System.out.println(maximum);
+        }
+
+        //System.out.println(maximum);//remove this line, because it will be in IF, we have check for >0
+    }
+}
