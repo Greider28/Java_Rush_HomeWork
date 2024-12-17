df91693d9da2b80443d3b355fc58bd116b19aa8b
Greider28 <73250074+Greider28@users.noreply.github.com>
2024-12-17 19:19:26 +0200

    Update Solution.java

--- src/com/javarush/test/level06/lesson05/task02/Solution.java
+++ src/com/javarush/test/level06/lesson05/task02/Solution.java
@@ -1,1 +1,25 @@
+package com.javarush.task.task06.task0602;
 
+/* 
+Пустые кошки, пустые псы
+В каждом классе Cat и Dog написать метод finalize, который выводит на экран текст о том, что такой-то объект уничтожен.
+*/
+
+public class Cat {
+    public static void main(String[] args) {
+
+    }
+
+    //напишите тут ваш код
+  protected void finalize() throws Throwable {
+        System.out.println("A Cat was destroyed");
+    }
+
+}
+
+class Dog {
+    //напишите тут ваш код
+  protected void finalize() throws Throwable {
+        System.out.println("A Dog was destroyed");
+    }
+}
