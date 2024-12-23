982c5fcbbf41b487cfa9d9306301d7007d007ddb
Greider28 <73250074+Greider28@users.noreply.github.com>
2024-12-23 20:40:42 +0200

    Update Solution.java

--- src/com/javarush/test/level06/lesson08/task03/Solution.java
+++ src/com/javarush/test/level06/lesson08/task03/Solution.java
@@ -1,1 +1,24 @@
+package com.javarush.task.task06.task0609;
 
+/* 
+Расстояние между двумя точками
+Реализовать статический метод double getDistance(x1, y1, x2, y2). Он должен вычислять расстояние между точками. Используй метод double Math.sqrt(double a), который вычисляет квадратный корень переданного параметра.
+*/
+
+public class Util {
+    public static double getDistance(int x1, int y1, int x2, int y2) {
+        //напишите тут ваш код
+      //start my code
+      //d = √((хА – хВ)2 + (уА – уВ)2)
+        double d;
+        double distance;
+        d = ((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+        distance = Math.sqrt(d);
+        return distance;
+      //end my code
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
