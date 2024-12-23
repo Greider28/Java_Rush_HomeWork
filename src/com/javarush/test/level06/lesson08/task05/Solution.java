6fa9cd039657046939dc995b4ffa4bb2c6ceea7f
Greider28 <73250074+Greider28@users.noreply.github.com>
2024-12-23 20:52:47 +0200

    Update Solution.java

--- src/com/javarush/test/level06/lesson08/task05/Solution.java
+++ src/com/javarush/test/level06/lesson08/task05/Solution.java
@@ -1,1 +1,34 @@
+package com.javarush.task.task06.task0611;
 
+/* 
+Класс StringHelper
+Сделать класс StringHelper, у которого будут 2 статических метода: String multiply(String s, int count) - возвращает строку повторенную count раз. String multiply(String s) - возвращает строку повторенную 5 раз.
+*/
+
+public class StringHelper {
+    public static String multiply(String text) {
+        String result = "";
+        //напишите тут ваш код
+      //start my code
+      for (int i = 0; i < 5; i++){
+            result += text;
+        }
+      //end my code
+        return result;
+    }
+
+    public static String multiply(String text, int count) {
+        String result = "";
+        //напишите тут ваш код
+      //start my code
+      for (int i = 0; i < count; i++){
+            result += text;
+        }
+      //end my code
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
