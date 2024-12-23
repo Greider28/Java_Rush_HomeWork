8b784ca8849b9f4dae551b5a374e9807999c8ccf
Greider28 <73250074+Greider28@users.noreply.github.com>
2024-12-23 21:45:51 +0200

    Update Solution.java

--- src/com/javarush/test/level06/lesson11/task03/Solution.java
+++ src/com/javarush/test/level06/lesson11/task03/Solution.java
@@ -1,1 +1,18 @@
+package com.javarush.task.task06.task0615;
+
+/* 
+Феншуй и статики
+Переставь один модификатор static, чтобы пример скомпилировался.
+*/
+
+public class Solution {
+
+    public static int A = 5;//add static
+    public int B = 2;
+    public int C = A * B;//remove static
+
+    public static void main(String[] args) {
+        A = 15;
+    }
+}
 
