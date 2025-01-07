647c5480213fdd7f3c01f2fd1536fae489e04a9f
Greider28 <73250074+Greider28@users.noreply.github.com>
2025-01-07 21:43:14 +0200

    Update Solution.java

--- src/com/javarush/test/level07/lesson04/task03/Solution.java
+++ src/com/javarush/test/level07/lesson04/task03/Solution.java
@@ -1,1 +1,30 @@
+package com.javarush.task.task07.task0703;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+Общение одиноких массивов
+1. Создать массив на 10 строк. 
+2. Создать массив на 10 чисел.
+3. Ввести с клавиатуры 10 строк, заполнить ими массив строк. 
+4. В каждую ячейку массива чисел записать длину строки из массива строк, индекс/номер ячейки которой совпадает с текущим индексом из массива чисел.
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+      BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String[] lines = new String[10];
+        int[] numbers = new int[10];
+
+        for (int i = 0; i < lines.length; i++){
+            lines[i] = reader.readLine();
+        }
+
+        for (int i = 0; i < numbers.length; i++){
+            numbers[i] = lines[i].length();
+            System.out.println(numbers[i]);
+        }
+    }
+}
