e03e2d2ad6a957edb17f15c60189c58d8bd9ae86
Greider28 <73250074+Greider28@users.noreply.github.com>
2025-01-07 21:36:04 +0200

    Update Solution.java

--- src/com/javarush/test/level07/lesson04/task02/Solution.java
+++ src/com/javarush/test/level07/lesson04/task02/Solution.java
@@ -1,1 +1,27 @@
+package com.javarush.task.task07.task0702;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+Массив из строчек в обратном порядке
+1. Создать массив на 10 строк. 
+2. Ввести с клавиатуры 8 строк и сохранить их в массив.
+3. Вывести содержимое всего массива (10 элементов) на экран в обратном порядке. Каждый элемент - с новой строки.
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+      BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String[] lines = new String[10];
+
+        for (int i = 0; i < 8; i++){
+            lines[i] = reader.readLine();
+        }
+
+        for (int i = lines.length - 1; i >= 0; i--){
+                System.out.println(lines[i]);
+        }
+    }
+}
