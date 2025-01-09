82b30128ce8013e6478221735b9db427ac1ddd05
Greider28 <73250074+Greider28@users.noreply.github.com>
2025-01-09 19:44:20 +0200

    Update Solution.java

--- src/com/javarush/test/level07/lesson06/task01/Solution.java
+++ src/com/javarush/test/level07/lesson06/task01/Solution.java
@@ -1,1 +1,29 @@
+package com.javarush.task.task07.task0707;
+
+import java.util.ArrayList;
+
+/* 
+Что за список такой?
+1. Создай список строк. 
+2. Добавь в него 5 различных строк. 
+3. Выведи его размер на экран. 
+4. Используя цикл выведи его содержимое на экран, каждое значение с новой строки.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+      ArrayList<String> list = new ArrayList<String>();
+        list.add("string1");
+        list.add("string2");
+        list.add("string3");
+        list.add("string4");
+        list.add("string5");
+        System.out.println(list.size());
+
+        for (int i = 0; i < list.size(); i++){
+            System.out.println(list.get(i));
+        }
+    }
+}
 
