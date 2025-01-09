c0638e980b4adf25e110fe67658cbe55aad15ae2
Greider28 <73250074+Greider28@users.noreply.github.com>
2025-01-09 20:12:17 +0200

    Update Solution.java

--- src/com/javarush/test/level07/lesson06/task02/Solution.java
+++ src/com/javarush/test/level07/lesson06/task02/Solution.java
@@ -1,1 +1,40 @@
+package com.javarush.task.task07.task0708;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/* 
+Самая длинная строка
+1. Создай список строк.
+2. Считай с клавиатуры 5 строк и добавь в список. 
+3. Используя цикл, найди самую длинную строку в списке.
+4. Выведи найденную строку на экран. Если таких строк несколько, выведи каждую с новой строки.
+*/
+
+public class Solution {
+    private static ArrayList<String> strings;
+
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+      strings = new ArrayList<String>();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        for (int i = 0; i < 5; i++){
+            strings.add(reader.readLine());
+        }
+
+        String maxString = strings.get(0);
+        for (int i = 0; i < strings.size(); i++){
+            if (strings.get(i).length() > maxString.length()){
+                maxString = strings.get(i);
+            }
+        }
+
+        for (int i = 0; i < strings.size(); i++){
+            if (strings.get(i).length() == maxString.length()){
+                System.out.println(strings.get(i));
+            }
+        }
+    }
+}
