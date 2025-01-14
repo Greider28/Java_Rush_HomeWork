3257c4fd1efc956a30f244ff762d172eb5ade980
Greider28 <73250074+Greider28@users.noreply.github.com>
2025-01-14 20:53:48 +0200

    Update Solution.java

--- src/com/javarush/test/level07/lesson09/task03/Solution.java
+++ src/com/javarush/test/level07/lesson09/task03/Solution.java
@@ -1,1 +1,28 @@
+package com.javarush.task.task07.task0715;
 
+import java.util.ArrayList;
+
+/* 
+Продолжаем мыть раму
+1. Создай список из слов "мама", "мыла", "раму".
+2. После каждого слова вставь в список строку, содержащую слово "именно". 
+3. Вывести результат на экран, каждый элемент списка с новой строки.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+      String[] strings = {"мама", "мыла", "раму"};
+        ArrayList<String> list = new ArrayList<String>();
+        for (int i = 0; i < strings.length; i++){
+            list.add(strings[i]);
+        }
+        for (int i = 0; i < list.size(); i++){
+            list.add(i + 1, "именно");
+            i++;
+        }
+        for (int i = 0; i < list.size(); i++){
+            System.out.println(list.get(i));
+        }
+    }
+}
