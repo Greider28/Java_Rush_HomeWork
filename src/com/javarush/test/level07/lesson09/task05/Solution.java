9acf9d31feb0a4ccd3761b0966aed3b045eea014
Greider28 <73250074+Greider28@users.noreply.github.com>
2025-01-14 21:56:23 +0200

    Update Solution.java

--- src/com/javarush/test/level07/lesson09/task05/Solution.java
+++ src/com/javarush/test/level07/lesson09/task05/Solution.java
@@ -14,14 +14,26 @@ import java.util.ArrayList;
 public class Solution {
     public static void main(String[] args) throws Exception {
         // Считать строки с консоли и объявить ArrayList list тут
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<String> list = new ArrayList<String>();
+        for (int i = 0; i < 10; i++){
+            list.add(reader.readLine());
+        }
 
         ArrayList<String> result = doubleValues(list);
 
         // Вывести на экран result
+        for (String string: result){
+            System.out.println(string);
+        }
     }
 
     public static ArrayList<String> doubleValues(ArrayList<String> list) {
         //напишите тут ваш код
-        return null;
+        for (int i = 0; i < list.size(); i++){
+            list.add(i + 1, list.get(i));
+            i++;
+        }
+        return null;//need return list;
     }
 }
