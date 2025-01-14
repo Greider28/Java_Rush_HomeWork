ead4de25d2e600366b303c767a65a14b89da04db
Greider28 <73250074+Greider28@users.noreply.github.com>
2025-01-14 22:07:34 +0200

    Update Solution.java

--- src/com/javarush/test/level07/lesson09/task06/Solution.java
+++ src/com/javarush/test/level07/lesson09/task06/Solution.java
@@ -1,1 +1,33 @@
+package com.javarush.task.task07.task0718;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/* 
+Проверка на упорядоченность
+1. Введи с клавиатуры 10 слов в список строк. 
+2. Определить, является ли список упорядоченным по возрастанию длины строки.
+3. В случае отрицательного ответа вывести на экран индекс первого элемента, нарушающего такую упорядоченность.
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        //напишите тут ваш код
+      BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<String> list = new ArrayList<String>();
+        for (int i = 0; i < 10; i++){
+            list.add(reader.readLine());
+        }
+        for (int i = 0; i < list.size() - 1; i++){
+            if (list.get(i).length() < list.get(i + 1).length()){
+
+            }
+            else {
+                System.out.println(list.indexOf(list.get(i + 1)));
+                break;
+            }
+        }
+    }
+}
