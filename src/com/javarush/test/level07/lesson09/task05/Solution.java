7f1a920725dc693d6f898e9bb90723b66598989a
Greider28 <73250074+Greider28@users.noreply.github.com>
2025-01-14 21:50:35 +0200

    Update Solution.java

--- src/com/javarush/test/level07/lesson09/task05/Solution.java
+++ src/com/javarush/test/level07/lesson09/task05/Solution.java
@@ -1,1 +1,27 @@
+package com.javarush.task.task07.task0717;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/* 
+Удваиваем слова
+1. Введи с клавиатуры 10 слов в список строк. 
+2. Метод doubleValues должен удваивать слова по принципу: "альфа", "бета", "гамма" -> "альфа", "альфа", "бета", "бета", "гамма", "гамма" 
+3. Выведи результат на экран, каждое значение с новой строки.
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        // Считать строки с консоли и объявить ArrayList list тут
+
+        ArrayList<String> result = doubleValues(list);
+
+        // Вывести на экран result
+    }
+
+    public static ArrayList<String> doubleValues(ArrayList<String> list) {
+        //напишите тут ваш код
+        return null;
+    }
+}
