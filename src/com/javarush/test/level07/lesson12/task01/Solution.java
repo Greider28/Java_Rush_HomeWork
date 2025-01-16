adc242484a6f124ddb3ff766552ecb8593c2fedc
Greider28 <73250074+Greider28@users.noreply.github.com>
2025-01-16 17:31:31 +0200

    Update Solution.java

--- src/com/javarush/test/level07/lesson12/task01/Solution.java
+++ src/com/javarush/test/level07/lesson12/task01/Solution.java
@@ -1,1 +1,29 @@
+package com.javarush.task.task07.task0719;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/* 
+Вывести числа в обратном порядке
+Ввести с клавиатуры 10 чисел и заполнить ими список.
+Вывести их в обратном порядке. Каждый элемент - с новой строки. Использовать только цикл for.  
+Подсказка: Не забудь импортировать класс: java.util.ArrayList;
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        //напишите тут ваш код
+      ArrayList<Integer> nums = new ArrayList<Integer>();
+        for (int i = 0; i < 10; i++){
+            nums.add(Integer.parseInt(reader.readLine()));
+        }
+        for (int i = nums.size() - 1; i >= 0; i--){
+            System.out.println(nums.get(i));
+        }
+    }
+}
 
