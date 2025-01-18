447dfa4ded8b24dcefe3d437e5bf3b84065c31aa
Greider28 <73250074+Greider28@users.noreply.github.com>
2025-01-18 16:46:47 +0200

    Update Solution.java

--- src/com/javarush/test/level08/lesson06/task01/Solution.java
+++ src/com/javarush/test/level08/lesson06/task01/Solution.java
@@ -1,1 +1,29 @@
+package com.javarush.task.task08.task0807;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+/* 
+LinkedList и ArrayList
+Нужно создать два списка - LinkedList и ArrayList.
+*/
+
+public class Solution {
+    public static Object createArrayList() {
+        //напишите тут ваш код
+      ArrayList<String> arrList = new ArrayList<>();
+        return arrList;
+
+    }
+
+    public static Object createLinkedList() {
+        //напишите тут ваш код
+      LinkedList<String> linkList = new LinkedList<>();
+        return linkList;
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
