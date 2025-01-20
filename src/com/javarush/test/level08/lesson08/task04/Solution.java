f17337ddc2528736767749f66ba2219fd6c43705
Greider28 <73250074+Greider28@users.noreply.github.com>
2025-01-20 19:15:46 +0200

    Update Solution.java

--- src/com/javarush/test/level08/lesson08/task04/Solution.java
+++ src/com/javarush/test/level08/lesson08/task04/Solution.java
@@ -1,1 +1,34 @@
+package com.javarush.task.task08.task0816;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+/* 
+Добрая Зинаида и летние каникулы
+Создать словарь (Map<String, Date>) и занести в него десять записей по принципу: "фамилия" - "дата рождения".
+Удалить из словаря всех людей, родившихся летом.
+*/
+
+public class Solution {
+    public static Map<String, Date> createMap() throws ParseException {
+        DateFormat dateFormat = new SimpleDateFormat("MMMMM d yyyy", Locale.ENGLISH);
+        Map<String, Date> map = new HashMap<>();
+        map.put("Смирнов", dateFormat.parse("MAY 1 2012"));
+
+        //напишите тут ваш код
+    }
+
+    public static void removeAllSummerPeople(Map<String, Date> map) {
+        //напишите тут ваш код
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
