2cda5e22db1f907c4b3f27ba5f9bb7c4104036b0
Greider28 <73250074+Greider28@users.noreply.github.com>
2025-01-23 20:07:24 +0200

    Update Solution.java

--- src/com/javarush/test/level08/lesson11/task03/Solution.java
+++ src/com/javarush/test/level08/lesson11/task03/Solution.java
@@ -1,1 +1,43 @@
+package com.javarush.task.task08.task0821;
 
+import java.util.HashMap;
+import java.util.Map;
+
+/* 
+Однофамильцы и тёзки
+1. Создать словарь Map (<String, String>) и добавить туда 10 человек в виде "Фамилия"-"Имя".
+2. Пусть среди этих 10 человек есть люди с одинаковыми именами. 
+3. Пусть среди этих 10 человек есть люди с одинаковыми фамилиями.
+4. Вывести содержимое Map на экран.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Map<String, String> map = createPeopleMap();
+        printPeopleMap(map);
+    }
+
+    public static Map<String, String> createPeopleMap() {
+        //напишите тут ваш код
+
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("lastname1", "firstname1");
+        map.put("lastname2", "firstname1");
+        map.put("lastname3", "firstname1");
+        map.put("lastname4", "firstname2");
+        map.put("lastname5", "firstname3");
+        map.put("lastname6", "firstname4");
+        map.put("lastname7", "firstname5");
+        map.put("lastname8", "firstname6");
+        map.put("lastname9", "firstname7");
+        map.put("lastname1", "firstname8");
+
+        return map;
+    }
+
+    public static void printPeopleMap(Map<String, String> map) {
+        for (Map.Entry<String, String> s : map.entrySet()) {
+            System.out.println(s.getKey() + " " + s.getValue());
+        }
+    }
+}
