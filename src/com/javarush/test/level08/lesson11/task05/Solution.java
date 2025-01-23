ed1335fd1ae1eda4d0060e140add55fd41579c9c
Greider28 <73250074+Greider28@users.noreply.github.com>
2025-01-23 20:39:19 +0200

    Update Solution.java

--- src/com/javarush/test/level08/lesson11/task05/Solution.java
+++ src/com/javarush/test/level08/lesson11/task05/Solution.java
@@ -1,1 +1,31 @@
+package com.javarush.task.task08.task0823;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/* 
+Омовение Рамы
+Написать программу, которая вводит с клавиатуры строку текста. 
+Программа заменяет в тексте первые буквы всех слов на заглавные.
+Вывести результат на экран.
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String string = reader.readLine();
+
+        //напишите тут ваш код
+      char[] stringArray = string.toCharArray();
+        stringArray[0] = Character.toUpperCase(stringArray[0]);
+        for (int i = 0; i < stringArray.length; i++){
+            char oneChar = stringArray[i];
+            if (oneChar == ' '){
+                stringArray[i + 1] = Character.toUpperCase(stringArray[i + 1]);
+            }
+        }
+        String stringArrayStr = new String(stringArray);
+        System.out.println(stringArrayStr);
+    }
+}
