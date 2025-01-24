afe744f9ecea90782d90e00cdd7e2e25563ed0bd
Greider28 <73250074+Greider28@users.noreply.github.com>
2025-01-24 18:00:35 +0200

    Update Solution.java

--- src/com/javarush/test/level08/lesson11/task08/Solution.java
+++ src/com/javarush/test/level08/lesson11/task08/Solution.java
@@ -1,1 +1,42 @@
+package com.javarush.task.task08.task0826;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+Пять победителей
+Создать массив на 20 чисел.
+Заполнить его числами с клавиатуры. 
+Вывести пять наибольших чисел. 
+Каждое значение с новой строки.
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int[] array = new int[20];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = Integer.parseInt(reader.readLine());
+        }
+
+        sort(array);
+
+        System.out.println(array[0]);
+        System.out.println(array[1]);
+        System.out.println(array[2]);
+        System.out.println(array[3]);
+        System.out.println(array[4]);
+    }
+
+    public static void sort(int[] array) {
+        //напишите тут ваш код
+      Integer[] arr = new Integer[array.length];
+        for (int i = 0; i < array.length; i++){
+            arr[i] = array[i];
+        }
+        Arrays.sort(arr, Collections.reverseOrder());
+        for (int i = 0; i < arr.length; i++){
+            array[i] = arr[i];
+        }
+    }
+}
