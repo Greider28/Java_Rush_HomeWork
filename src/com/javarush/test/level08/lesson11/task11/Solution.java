8bf0e37d28b2eeec9186b38b266b4e59da937376
Greider28 <73250074+Greider28@users.noreply.github.com>
2025-01-24 19:42:57 +0200

    Update Solution.java

--- src/com/javarush/test/level08/lesson11/task11/Solution.java
+++ src/com/javarush/test/level08/lesson11/task11/Solution.java
@@ -1,1 +1,76 @@
+package com.javarush.task.task08.task0829;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+
+/* 
+Модернизация ПО
+Задача: Программа определяет, какая семья (фамилию) живёт в доме с указанным номером.
+Новая задача: Программа должна работать не с номерами домов, а с городами:
+Пример ввода: 
+Москва
+Ивановы
+Киев
+Петровы 
+Лондон
+Абрамовичи
+Лондон
+Пример вывода:
+Абрамовичи
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        /*BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        List<String> list = new ArrayList<>();
+        while (true) {
+            String family = reader.readLine();
+            if (family.isEmpty()) {
+                break;
+            }
+
+            list.add(family);
+        }
+
+        // Read the house number
+        int houseNumber = Integer.parseInt(reader.readLine());
+
+        if (0 <= houseNumber && houseNumber < list.size()) {
+            String familyName = list.get(houseNumber);
+            System.out.println(familyName);
+        }
+      */
+      BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        Map<String, String> map = new HashMap<String, String>();
+        while (true) {
+            String city = reader.readLine();
+            if (city.isEmpty()) {
+                break;
+            }
+            String family = reader.readLine();
+            if (family.isEmpty()) {
+                break;
+            }
+
+            map.put(city, family);
+        }
+
+        // Read the family
+        while (true) {
+            String citySearch = reader.readLine();
+            if (citySearch.isEmpty()) {
+                break;
+            }
+
+            System.out.println(map.get(citySearch));
+
+        }
+    }
+}
