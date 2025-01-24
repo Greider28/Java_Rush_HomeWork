09eef9b134a0a7c81c96cadb9b1bb12772b904c3
Greider28 <73250074+Greider28@users.noreply.github.com>
2025-01-24 18:58:47 +0200

    Update Solution.java

--- src/com/javarush/test/level08/lesson11/task10/Solution.java
+++ src/com/javarush/test/level08/lesson11/task10/Solution.java
@@ -1,1 +1,20 @@
+package com.javarush.task.task08.task0828;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Номер месяца
+Программа вводит с клавиатуры имя месяца и выводит его номер на экран в виде:
+"May is the 5 month". 
+Используй коллекции.
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        //напишите тут ваш код
+    }
+}
