eb77f1ced163a30cf855ad88f0e5aea2266f3897
Greider28 <73250074+Greider28@users.noreply.github.com>
2025-01-24 17:41:41 +0200

    Update Solution.java

--- src/com/javarush/test/level08/lesson11/task07/Solution.java
+++ src/com/javarush/test/level08/lesson11/task07/Solution.java
@@ -1,1 +1,25 @@
+package com.javarush.task.task08.task0825;
 
+/* 
+Модификатор запутался
+Переставь один модификатор static, чтобы пример скомпилировался.
+*/
+
+public class Solution {
+    public static int A = 5;//add static
+    public static int B = 2;
+
+    public int C = A * B;
+    public static int D = B * A;
+
+    public static void main(String[] args) {
+    }
+
+    public int getValue() {//remove static
+        return D;
+    }
+
+    public int getValue2() {
+        return C;
+    }
+}
