e4c3c87b2c8244d447a09c1cfa49bd4065b009d6
Greider28 <73250074+Greider28@users.noreply.github.com>
2025-01-28 18:33:25 +0200

    Update Solution.java

--- src/com/javarush/test/level09/lesson02/task05/Solution.java
+++ src/com/javarush/test/level09/lesson02/task05/Solution.java
@@ -1,1 +1,20 @@
+package com.javarush.task.task09.task0905;
 
+/* 
+Там, в синих глубинах стек-трейса…
+Написать метод, который возвращает результат - глубину его стек-трейса - количество методов в нем (количество элементов в списке). 
+Это же число метод должен выводить на экран.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int deep = getStackTraceDepth();
+    }
+
+    public static int getStackTraceDepth() {
+        //напишите тут ваш код
+      StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
+        System.out.println(stackTraceElements.length);
+        return stackTraceElements.length;
+    }
+}
