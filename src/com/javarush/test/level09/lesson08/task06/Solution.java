9a453f4ea3814dbfa6c5e9e5c1df5736622fcaab
Greider28 <73250074+Greider28@users.noreply.github.com>
2025-02-03 20:13:30 +0200

    Update Solution.java

--- src/com/javarush/test/level09/lesson08/task06/Solution.java
+++ src/com/javarush/test/level09/lesson08/task06/Solution.java
@@ -1,1 +1,24 @@
+package com.javarush.task.task09.task0918;
 
+/* 
+Все свои, даже исключения
+Есть четыре класса MyException, MyException2, MyException3, MyException4.
+Унаследуй классы так, чтобы у тебя появилось любые два checked исключения и любые два unchecked исключения.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    static class MyException {
+    }
+
+    static class MyException2 {
+    }
+
+    static class MyException3 {
+    }
+
+    static class MyException4 {
+    }
+}
