2ebdff207bd09232d2cdaa5b54bc02ebec29c065
Greider28 <73250074+Greider28@users.noreply.github.com>
2025-02-11 21:12:55 +0200

    Update Solution.java

--- src/com/javarush/test/level09/lesson11/task07/Solution.java
+++ src/com/javarush/test/level09/lesson11/task07/Solution.java
@@ -1,1 +1,25 @@
+package com.javarush.task.task09.task0925;
 
+/* 
+Статики не на своем месте
+Расставь модификаторы static так, чтобы пример скомпилировался.
+*/
+
+public class Solution {
+    public static int A = 5;// add static
+    public static int B = 2 * A;// add static
+    public int C = A * B;
+    public static int D = A * B;// add static
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.A = 5;
+
+        Solution.D = 5;
+    }
+
+    public int getA() {
+        return A;
+    }
+
+}
