8aea8f3a5f96638b019b41f1287e964962618144
Greider28 <73250074+Greider28@users.noreply.github.com>
2025-02-11 20:31:29 +0200

    Update Solution.java

--- src/com/javarush/test/level09/lesson11/task04/Solution.java
+++ src/com/javarush/test/level09/lesson11/task04/Solution.java
@@ -1,1 +1,27 @@
+package com.javarush.task.task09.task0922;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+/* 
+Какое сегодня число?
+Ввести с клавиатуры дату в формате "2013-08-18"
+Вывести на экран введенную дату в виде "AUG 18, 2013". 
+Воспользоваться объектом Date и SimpleDateFormat.
+*/
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+      BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String enterDate = reader.readLine();
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH);
+        Date date = formatter.parse(enterDate);
+        SimpleDateFormat newFormat = new SimpleDateFormat("MMM dd, yyyy", Locale.ENGLISH);
+        System.out.println(newFormat.format(date).toUpperCase());
+    }
+}
