fc4368f1b6589f7b02c428d82a6e0aa1d1f04fd2
Greider28 <73250074+Greider28@users.noreply.github.com>
2025-02-11 20:00:06 +0200

    Update Solution.java

--- src/com/javarush/test/level09/lesson11/task03/Solution.java
+++ src/com/javarush/test/level09/lesson11/task03/Solution.java
@@ -1,1 +1,37 @@
+package com.javarush.task.task09.task0921;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Метод в try..catch
+Написать программу, которая будет вводить числа с клавиатуры.
+Код по чтению чисел с клавиатуры должен быть в методе readData.
+Код внутри readData обернуть в try..catch. 
+Если пользователь ввёл какой-то текст, вместо ввода числа, то метод должен перехватить исключение и вывести на экран все ранее введенные числа в качестве результата.
+Числа выводить с новой строки сохраняя порядок ввода.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        readData();
+    }
+
+    public static void readData() {
+        //напишите тут ваш код
+      ArrayList<Integer> arrList = new ArrayList<Integer>();
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            while (true) {
+                int a = Integer.parseInt(reader.readLine());
+                arrList.add(a);
+            }
+        } catch (Exception e) {
+            for (Integer num: arrList){
+                System.out.println(num);
+            }
+        }
+    }
+}
