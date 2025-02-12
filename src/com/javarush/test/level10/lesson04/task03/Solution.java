c0dd5806e9b72daaf9570c64808c8dd45566e338
Greider28 <73250074+Greider28@users.noreply.github.com>
2025-02-12 20:35:40 +0200

    Update Solution.java

--- src/com/javarush/test/level10/lesson04/task03/Solution.java
+++ src/com/javarush/test/level10/lesson04/task03/Solution.java
@@ -1,1 +1,18 @@
+package com.javarush.task.task10.task1003;
 
+/* 
+Задача №3 на преобразование целых типов
+Добавь одну операцию по преобразованию типа, чтобы получился ответ: b = 0.
+float f = (float)128.50;
+int i = (int)f; 
+int b = (int)(i + f);
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        float f = (float) 128.50;
+        int i = (int) f;
+        int b = (int) (byte) (i + f);
+        System.out.println(b);
+    }
+}
