458d7b70b8078f34b55cf99d58bdf3d0f0c3e493
Greider28 <73250074+Greider28@users.noreply.github.com>
2025-02-12 20:47:11 +0200

    Update Solution.java

--- src/com/javarush/test/level10/lesson04/task04/Solution.java
+++ src/com/javarush/test/level10/lesson04/task04/Solution.java
@@ -1,1 +1,19 @@
+package com.javarush.task.task10.task1004;
 
+/* 
+Задача №4 на преобразование целых типов
+Добавь одну операцию по преобразованию типа, чтобы на экран вывелось 9.
+short number = 9;
+char zero = '0';
+int nine = (zero + number);
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        short number = 9;
+        char zero = '0';
+        int nine = (zero + number);
+        // old code System.out.println(nine);
+      System.out.println((char) nine);
+    }
+}
