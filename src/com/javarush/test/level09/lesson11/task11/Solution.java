0a9e8bc7d78506eee9dce492924b9a5b307fc177
Greider28 <73250074+Greider28@users.noreply.github.com>
2025-02-12 02:26:27 +0200

    Update Solution.java

--- src/com/javarush/test/level09/lesson11/task11/Solution.java
+++ src/com/javarush/test/level09/lesson11/task11/Solution.java
@@ -32,11 +32,10 @@ public class Solution {
       */
       BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-
             String sourceFileName = reader.readLine();
             try {
                 InputStream fileInputStream = getInputStream(sourceFileName);
-            } catch(FileNotFoundException e){
+            } catch (FileNotFoundException e){
                 System.out.println("Файл не существует.");
                 sourceFileName = reader.readLine();
 
@@ -52,6 +51,32 @@ public class Solution {
 
             fileInputStream.close();
             fileOutputStream.close();
+        /* main from the JR solution
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String sourceFileName = reader.readLine();
+
+        InputStream fileInputStream = null;
+        try {
+            fileInputStream = getInputStream(sourceFileName);
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не существует.");
+            sourceFileName = reader.readLine();
+            fileInputStream = getInputStream(sourceFileName);
+        }
+
+        String destinationFileName = reader.readLine();
+        OutputStream fileOutputStream = getOutputStream(destinationFileName);
+
+        while (fileInputStream.available() > 0) {
+            int data = fileInputStream.read();
+            fileOutputStream.write(data);
+        }
+
+        fileInputStream.close();
+        fileOutputStream.close();
+
+        */
     }
 
     public static InputStream getInputStream(String fileName) throws IOException {
