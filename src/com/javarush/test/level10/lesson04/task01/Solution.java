de7a80b8f33b410e902975c7c183c65a5c2b29c4
Greider28 <73250074+Greider28@users.noreply.github.com>
2025-02-12 20:22:58 +0200

    Update Solution.java

--- src/com/javarush/test/level10/lesson04/task01/Solution.java
+++ src/com/javarush/test/level10/lesson04/task01/Solution.java
@@ -1,1 +1,22 @@
+package com.javarush.task.task10.task1001;
 
+/* 
+Задача №1 на преобразование целых типов
+Правильно расставь операторы приведения типа, чтобы получился ответ: d > 0.
+int a = 0;
+int b = (byte) a + 46;
+byte c = (byte) (a*b);
+double f = (char) 1234.15;
+long d = (short) (a + f / c + b);
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+         int a = 0;
+        int b = (byte) a + 46;
+        byte c = (byte) (a * b);
+        double f = 1234.15;
+        long d = (long) ((double) a + f / (double) c + b);
+        System.out.println(d);
+    }
+}
