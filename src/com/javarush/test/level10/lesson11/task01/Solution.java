a2183e9bdd05b4705dd754f259816d93a6e32653
Greider28 <73250074+Greider28@users.noreply.github.com>
2025-02-25 19:52:41 +0200

    Update Solution.java

--- src/com/javarush/test/level10/lesson11/task01/Solution.java
+++ src/com/javarush/test/level10/lesson11/task01/Solution.java
@@ -1,1 +1,17 @@
+package com.javarush.task.task10.task1008;
+
+/* 
+Правильный ответ: d = 2.941
+Добавь в метод main() одну операцию по преобразованию типа, чтобы получился ответ: d = 2.941.
+Пример вывода: 2.9411764705882355
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int a = 50;
+        int b = 17;
+        double d = (double) a / b; //add (double)
+        System.out.println(d);
+    }
+}
 
