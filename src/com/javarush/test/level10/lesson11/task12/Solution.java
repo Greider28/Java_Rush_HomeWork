40f0e034583014e3cb17810a3df6ae7a75a53b1f
Greider28 <73250074+Greider28@users.noreply.github.com>
2025-02-26 19:58:22 +0200

    Update Solution.java

--- src/com/javarush/test/level10/lesson11/task12/Solution.java
+++ src/com/javarush/test/level10/lesson11/task12/Solution.java
@@ -1,1 +1,40 @@
+package com.javarush.task.task10.task1019;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+/* 
+Функциональности маловато!
+Программа вводит с клавиатуры пару (число и строку) и выводит их на экран. Нужно сделать так, чтобы программа вводила с клавиатуры пары (число и строку) и сохраняла их в HashMap.
+Ключевые требования: 
+Пустая строка - конец ввода данных.
+Числа могут повторяться.
+Строки всегда уникальны.
+Введенные данные не должны потеряться!
+Затем программа должна выводить содержание HashMap на экран. Каждую пару - с новой строки.
+Пример ввода: 
+1 
+Мама
+2 
+Рама
+1
+Мыла
+Пример вывода:
+1 Мыла 
+2 Рама 
+1 Мама
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int id = Integer.parseInt(reader.readLine());
+        String name = reader.readLine();
+
+        System.out.println("Id=" + id + " Name=" + name);
+    }
+}
