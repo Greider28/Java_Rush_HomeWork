d75dd615aa445102378fd68845f0ebce5ef755ef
Greider28 <73250074+Greider28@users.noreply.github.com>
2025-02-26 19:30:16 +0200

    Update Solution.java

--- src/com/javarush/test/level10/lesson11/task08/Solution.java
+++ src/com/javarush/test/level10/lesson11/task08/Solution.java
@@ -1,1 +1,37 @@
+package com.javarush.task.task10.task1015;
 
+import java.util.ArrayList;
+
+/* 
+Массив списков строк
+Создай массив, элементами которого будут списки строк. Заполни массив любыми данными и выведи их на экран.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        ArrayList<String>[] arrayOfStringList = createList();
+        printList(arrayOfStringList);
+    }
+
+    public static ArrayList<String>[] createList() {
+        //напишите тут ваш код
+        ArrayList<String>[] array = new ArrayList[3];
+        ArrayList<String> list1 = new ArrayList<String>();
+        for (int i = 0; i < 5; i++){
+            list1.add("abc");
+        }
+        for (int i = 0; i < array.length; i++){
+            array[i] = list1;
+        }
+
+        return array;
+    }
+
+    public static void printList(ArrayList<String>[] arrayOfStringList) {
+        for (ArrayList<String> list : arrayOfStringList) {
+            for (String string : list) {
+                System.out.println(string);
+            }
+        }
+    }
+}
