a50de1fc3d0b1a4d0df49e29f2c76acb4186abf2
Greider28 <73250074+Greider28@users.noreply.github.com>
2025-02-26 19:41:05 +0200

    Update Solution.java

--- src/com/javarush/test/level10/lesson11/task09/Solution.java
+++ src/com/javarush/test/level10/lesson11/task09/Solution.java
@@ -1,1 +1,52 @@
+package com.javarush.task.task10.task1016;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+/* 
+Одинаковые слова в списке
+Введи с клавиатуры 20 слов в список. Посчитай, сколько раз каждое из них встречается в списке.
+Результат нужно представить в виде словаря Map<String, Integer>, где первый параметр - уникальное слово, а второй - число раз, сколько раз данное слово встречалось в списке.
+Выведи содержимое словаря на экран.
+В тестах регистр (большая/маленькая буква) влияет на результат.
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        ArrayList<String> words = new ArrayList<String>();
+        for (int i = 0; i < 20; i++) {
+            words.add(reader.readLine());
+        }
+
+        Map<String, Integer> map = countWords(words);
+
+        for (Map.Entry<String, Integer> pair : map.entrySet()) {
+            System.out.println(pair.getKey() + " " + pair.getValue());
+        }
+    }
+
+    public static Map<String, Integer> countWords(ArrayList<String> list) {
+        HashMap<String, Integer> result = new HashMap<String, Integer>();
+
+        //напишите тут ваш код
+      for (int i = 0; i < list.size(); i++){
+            int count = 0;
+            for (int j = 0; j < list.size(); j++){
+                if (list.get(i).equals(list.get(j))){
+                    count++;
+                }
+            }
+            result.put(list.get(i), count);
+        }
+
+
+        return result;
+    }
+
+}
 
