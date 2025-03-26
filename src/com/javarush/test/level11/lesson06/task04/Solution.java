cbbb6d125f0b2ac96f4bd44731ac6710fb18979b
Greider28 <73250074+Greider28@users.noreply.github.com>
2025-03-26 19:43:06 +0200

    Update Solution.java

--- src/com/javarush/test/level11/lesson06/task04/Solution.java
+++ src/com/javarush/test/level11/lesson06/task04/Solution.java
@@ -1,1 +1,27 @@
+package com.javarush.task.task11.task1104;
 
+/* 
+Хороши работнички
+Напиши четыре класса: Employee (сотрудник), Manager (управляющий), Chief (директор) и Secretary (секретарь). Унаследуй управляющего, директора и секретаря от сотрудника.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Manager {
+
+    }
+
+    public class Chief {//extends Employee
+
+    }
+
+    public class Employee {//extends Employee
+
+    }
+
+    public class Secretary {//extends Employee
+
+    }
+}
