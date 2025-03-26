95d9e8f1e1c38b8842abe18f59270d52ec4d7e5c
Greider28 <73250074+Greider28@users.noreply.github.com>
2025-03-26 19:37:28 +0200

    Update Solution.java

--- src/com/javarush/test/level11/lesson06/task01/Solution.java
+++ src/com/javarush/test/level11/lesson06/task01/Solution.java
@@ -1,1 +1,19 @@
+package com.javarush.task.task11.task1101;
 
+/* 
+Лошадь и Пегас
+Отредактируй два класса — Horse (лошадь) и Pegasus (пегас). Унаследуй пегаса от лошади.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Horse {
+
+    }
+
+    public class Pegasus { //public class Pegasus extends Horse
+
+    }
+}
