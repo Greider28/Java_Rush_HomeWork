61a048834812a36494e4ca323d09d8149b3baf5d
Greider28 <73250074+Greider28@users.noreply.github.com>
2025-03-26 19:59:17 +0200

    Update Solution.java

--- src/com/javarush/test/level11/lesson08/task03/Solution.java
+++ src/com/javarush/test/level11/lesson08/task03/Solution.java
@@ -1,1 +1,39 @@
+package com.javarush.task.task11.task1108;
 
+/* 
+Неприступный кот
+Скрой все внутренние переменные класса Cat, а также методы, позволяющие менять внутреннее состояние объектов класса Cat.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Cat {
+        public String name;//private
+        public int age;//private
+        public int weight;//private
+
+        public Cat(String name, int age, int weight) {
+            this.name = name;
+            this.age = age;
+            this.weight = weight;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {//private
+            this.name = name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {//private
+            this.age = age;
+        }
+    }
+}
