16ce9b21d3350609a8a957ee32253331f27642d4
Greider28 <73250074+Greider28@users.noreply.github.com>
2025-03-31 20:57:28 +0300

    Update Solution.java

--- src/com/javarush/test/level11/lesson11/task11/Solution.java
+++ src/com/javarush/test/level11/lesson11/task11/Solution.java
@@ -1,1 +1,56 @@
+package com.javarush.task.task11.task1121;
 
+/* 
+Чужой код, незнакомый
+Исправь наследование в классах Cat, Dog, Pet, House, Airplane, чтобы программа компилировалась и работала.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Pet extends House {
+
+    }
+
+    public class Cat extends Airplane {
+
+    }
+
+    public class Dog extends Cat {
+
+    }
+
+    public class House extends Dog {
+
+    }
+
+    public class Airplane {
+
+    }
+
+  /*
+  public static void main(String[] args) {
+    }
+
+    public class Pet {
+
+    }
+
+    public class Cat extends Pet {
+
+    }
+
+    public class Dog extends Pet {
+
+    }
+
+    public class House {
+
+    }
+
+    public class Airplane {
+
+    }
+  */
+}
