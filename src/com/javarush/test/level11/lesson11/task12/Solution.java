8cab1e48f30d7da8b7271390e60ac140534564df
Greider28 <73250074+Greider28@users.noreply.github.com>
2025-03-31 20:59:54 +0300

    Update Solution.java

--- src/com/javarush/test/level11/lesson11/task12/Solution.java
+++ src/com/javarush/test/level11/lesson11/task12/Solution.java
@@ -1,1 +1,57 @@
 
+package com.javarush.task.task11.task1122;
+
+/* 
+Помощь шахматной школе
+Добавь общий базовый класс ChessPieces к классам фигур (фигуры из шахмат).
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class King {
+    }
+
+    public class Queen {
+    }
+
+    public class Rook {
+    }
+
+    public class Knight {
+    }
+
+    public class Bishop {
+    }
+
+    public class Pawn {
+    }
+
+  /*
+  public static void main(String[] args) {
+    }
+
+    public class King extends ChessPieces{
+    }
+
+    public class Queen extends ChessPieces{
+    }
+
+    public class Rook extends ChessPieces{
+    }
+
+    public class Knight extends ChessPieces{
+    }
+
+    public class Bishop extends ChessPieces{
+    }
+
+    public class Pawn extends ChessPieces{
+    }
+    
+    public class ChessPieces {
+
+    }
+  */
+}
