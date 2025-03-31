db3fe8a0cf64429af9d2db183a7a225b2427f552
Greider28 <73250074+Greider28@users.noreply.github.com>
2025-03-31 20:53:07 +0300

    Update Solution.java

--- src/com/javarush/test/level11/lesson11/task09/Solution.java
+++ src/com/javarush/test/level11/lesson11/task09/Solution.java
@@ -1,1 +1,47 @@
+package com.javarush.task.task11.task1119;
 
+/* 
+Четвертая правильная «цепочка наследования»
+Расставь правильно «цепочку наследования» в классах: House (дом), Cat (кот), Dog (собака), Car (машина).
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class House extends Cat {
+
+    }
+
+    public class Cat extends Car {
+
+    }
+
+    public class Car extends Dog {
+
+    }
+
+    public class Dog extends House {
+
+    }
+  /*
+  public static void main(String[] args) {
+    }
+
+    public class House {
+
+    }
+
+    public class Cat {
+
+    }
+
+    public class Car {
+
+    }
+
+    public class Dog {
+
+    }
+  */
+}
