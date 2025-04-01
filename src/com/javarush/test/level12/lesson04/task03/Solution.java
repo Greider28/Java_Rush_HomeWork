bc642ea34cfd503630d4f00a8d822222f27ccc75
Greider28 <73250074+Greider28@users.noreply.github.com>
2025-04-01 19:37:19 +0300

    Update Solution.java

--- src/com/javarush/test/level12/lesson04/task03/Solution.java
+++ src/com/javarush/test/level12/lesson04/task03/Solution.java
@@ -1,1 +1,29 @@
+package com.javarush.task.task12.task1208;
 
+/* 
+Свобода печати
+Напиши пять методов print() с разными параметрами.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    //Напишите тут ваши методы
+  public static void print(int a){
+
+    }
+    public static void print(Integer a){
+
+    }
+    public static void print(String a){
+
+    }
+    public static void print(double a){
+
+    }
+    public static void print(char a){
+
+    }
+}
