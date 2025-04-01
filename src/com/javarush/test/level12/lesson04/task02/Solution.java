f7d198b99594317e628940aa21ea21d807d127bb
Greider28 <73250074+Greider28@users.noreply.github.com>
2025-04-01 19:35:13 +0300

    Update Solution.java

--- src/com/javarush/test/level12/lesson04/task02/Solution.java
+++ src/com/javarush/test/level12/lesson04/task02/Solution.java
@@ -1,1 +1,24 @@
+package com.javarush.task.task12.task1207;
 
+/* 
+Int и Integer
+Реализуй два метода: print(int) и print(Integer). Напиши такой код в методе main(), чтобы вызвались оба.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+      int first = 5;
+        Integer second = 8;
+        print(first);
+        print(second);
+
+    }
+
+    //Напишите тут ваши методы
+  public static void print(int a){
+
+    }
+    public static void print(Integer b){
+
+    }
+}
