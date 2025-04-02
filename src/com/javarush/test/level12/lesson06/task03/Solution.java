bee32a3bfe7dbd7acf3bd6977d954e0db946e945
Greider28 <73250074+Greider28@users.noreply.github.com>
2025-04-02 19:04:32 +0300

    Update Solution.java

--- src/com/javarush/test/level12/lesson06/task03/Solution.java
+++ src/com/javarush/test/level12/lesson06/task03/Solution.java
@@ -1,1 +1,21 @@
+package com.javarush.task.task12.task1213;
 
+/* 
+«Исправь код», часть 2
+Исправь код, чтобы программа компилировалась.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static class Pet {//public static abstract class Pet
+        public String getName() {
+            return "Я - котенок";
+        }
+
+        public Pet getChild();//public abstract Pet getChild()
+    }
+
+}
