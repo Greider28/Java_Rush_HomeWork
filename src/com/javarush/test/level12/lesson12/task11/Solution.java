96d213fcc06ee50c4a9a76029ec809342e5e3cc4
Greider28 <73250074+Greider28@users.noreply.github.com>
2025-04-03 20:05:18 +0300

    Update Solution.java

--- src/com/javarush/test/level12/lesson12/task11/Solution.java
+++ src/com/javarush/test/level12/lesson12/task11/Solution.java
@@ -1,1 +1,35 @@
+package com.javarush.task.task12.task1231;
 
+/* 
+Ненужные абстракции
+Необходимо расставить правильно ключевые слова abstract, чтобы программа компилировалась. Добавь там, где нужно, и удали там, где они не нужны.
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Horse horse = new Pegasus();
+        horse.run();
+    }
+
+    public static interface CanFly {
+        public abstract void fly();
+    }
+
+    public static abstract class Horse {//public static class Horse
+        public void run() {
+
+        }
+    }
+
+    public static class Pegasus extends Horse implements CanFly {
+        public abstract void fly() {//public void fly()
+
+        }
+    }
+
+    public static class SwimmingPegasus extends Pegasus {//public static abstract class SwimmingPegasus extends Pegasus
+        public void swim();//public abstract void swim();
+    }
+
+}
