95f795536c56fcc66337dabd7a12d10d77fe716a
Greider28 <73250074+Greider28@users.noreply.github.com>
2025-04-03 19:57:50 +0300

    Update Solution.java

--- src/com/javarush/test/level12/lesson12/task09/Solution.java
+++ src/com/javarush/test/level12/lesson12/task09/Solution.java
@@ -1,1 +1,33 @@
+package com.javarush.task.task12.task1229;
 
+/* 
+Родитель класса CTO
+Добавь такой класс-родитель к классу CTO (технический директор), чтобы класс перестал быть абстрактным. Добавлять/реализовывать методы в классе CTO запрещается.
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        CTO cto = new CTO();
+        System.out.println(cto);
+    }
+
+    public static interface Businessman {
+        public void workHard();
+    }
+
+  //  
+  public static class CTO implements Businessman {
+
+    }
+  /*
+  public static class CTO extends Human implements Businessman {
+
+    }
+    public static class Human implements Businessman {
+        public void workHard(){
+
+        }
+    }
+  */
+}
