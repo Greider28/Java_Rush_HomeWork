18ba1faa923afd72e3562b3a44f3ea6437ea82ff
Greider28 <73250074+Greider28@users.noreply.github.com>
2025-04-03 19:42:55 +0300

    Update Solution.java

--- src/com/javarush/test/level12/lesson12/task06/Solution.java
+++ src/com/javarush/test/level12/lesson12/task06/Solution.java
@@ -1,1 +1,76 @@
+package com.javarush.task.task12.task1226;
 
+/* 
+Лазать, летать и бегать
+Вот что тебе нужно сделать в этой задаче: 
+Внутри класса Solution создай интерфейс public interface CanFly (летать) с методом void fly().
+Внутри класса Solution создай интерфейс public interface CanClimb (лазить по деревьям) с методом void climb().
+Внутри класса Solution создай интерфейс public interface CanRun (бегать) с методом void run().
+Подумай логически, какие именно интерфейсы нужно добавить для каждого класса.
+Добавь интерфейсы классам Cat (кот), Dog (собака), Tiger (тигр), Duck (Утка).
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public class Cat {
+    }
+
+    public class Dog {
+    }
+
+    public class Tiger extends Cat {
+    }
+
+    public class Duck {
+    }
+  /*
+  public class Cat implements CanClimb, CanRun{
+        public void climb(){
+
+        };
+        public void run(){
+
+        };
+    }
+
+    public class Dog implements CanRun{
+        public void run(){
+
+        }
+    }
+
+    public class Tiger extends Cat{
+        public void climb(){
+
+        }
+        public void run(){
+
+        }
+    }
+
+    public class Duck implements CanRun, CanFly{
+        public void run(){
+
+        }
+        public void fly(){
+
+        }
+    }
+
+    public interface CanFly{
+        void fly();
+    }
+
+    public interface CanClimb{
+        void climb();
+    }
+
+    public interface CanRun{
+        void run();
+    }
+  */
+}
