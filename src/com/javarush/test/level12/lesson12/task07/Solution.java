78666c093442f24788733b61c3ecb362dc7e1c0f
Greider28 <73250074+Greider28@users.noreply.github.com>
2025-04-03 19:46:44 +0300

    Update Solution.java

--- src/com/javarush/test/level12/lesson12/task07/Solution.java
+++ src/com/javarush/test/level12/lesson12/task07/Solution.java
@@ -1,1 +1,68 @@
+package com.javarush.task.task12.task1227;
 
+/* 
+CanFly, CanRun, CanSwim для классов Duck, Penguin, Toad
+Есть интерфейсы CanFly (летать), CanSwim (плавать), CanRun (бегать). Добавь эти интерфейсы классам Duck (утка), Penguin (пингвин), Toad (жаба)
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public interface CanFly {
+        public void fly();
+    }
+
+    public interface CanRun {
+        public void run();
+    }
+
+    public interface CanSwim {
+        public void swim();
+    }
+
+  //  
+  public class Duck {
+
+    }
+
+    public class Penguin {
+
+    }
+
+    public class Toad {
+
+    }
+  /*
+  public class Duck implements CanFly, CanRun, CanSwim{
+        public void fly(){
+
+        }
+        public void run(){
+
+        }
+        public void swim(){
+
+        }
+
+    }
+
+    public class Penguin implements CanRun, CanSwim{
+        public void run(){
+
+        }
+        public void swim(){
+
+        }
+
+    }
+
+    public class Toad implements CanSwim{
+        public void swim(){
+
+        }
+
+    }
+  */
+}
