f6d61b0d0b634eb05944e2d1f385c1234689a5c9
Greider28 <73250074+Greider28@users.noreply.github.com>
2025-04-07 19:34:32 +0300

    Update Solution.java

--- src/com/javarush/test/level13/lesson02/task04/Solution.java
+++ src/com/javarush/test/level13/lesson02/task04/Solution.java
@@ -1,1 +1,31 @@
+package com.javarush.task.task13.task1304;
+
+/* 
+Selectable и Updatable
+Создай класс Screen и реализуй в нем интерфейсы Selectable и Updatable. Не забудь реализовать методы!
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+    }
+
+    interface Selectable {
+        void onSelect();
+    }
+
+    interface Updatable {
+        void refresh();
+    }
+
+    //напишите тут ваш класс
+  public class Screen implements Selectable, Updatable{
+        public void onSelect(){
+            System.out.println("select");
+        }
+        public void refresh(){
+            System.out.println("refresh update");
+        }
+
+    }
+}
 
