0d052cd09eccda77076d4658f8338e9d70eb100d
Greider28 <73250074+Greider28@users.noreply.github.com>
2025-04-07 19:27:44 +0300

    Update Solution.java

--- src/com/javarush/test/level13/lesson02/task02/Solution.java
+++ src/com/javarush/test/level13/lesson02/task02/Solution.java
@@ -1,1 +1,47 @@
+package com.javarush.task.task13.task1302;
 
+/* 
+Пиво-2. Возвращение
+Добавь к классу AlcoholicBeer интерфейс Drink и реализуй все его методы.
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        Drink beer = new AlcoholicBeer();
+        System.out.println(beer.toString());
+    }
+
+    public interface Drink {
+        boolean isAlcoholic();
+    }
+
+    public static class AlcoholicBeer {
+        @Override
+        public String toString() {
+            if (isAlcoholic()) {
+                return "Напиток алкогольный";
+            } else {
+                return "Напиток безалкогольный";
+            }
+
+        }
+
+    }
+  /*
+  public static class AlcoholicBeer implements Drink{
+        public boolean isAlcoholic(){
+            return true;
+        }
+        @Override
+        public String toString() {
+            if (isAlcoholic()) {
+                return "Напиток алкогольный";
+            } else {
+                return "Напиток безалкогольный";
+            }
+
+        }
+
+    }
+  */
+}
