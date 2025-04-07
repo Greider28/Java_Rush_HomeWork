a7a7491887e5563e5ff8c765b454b20f6f382ffe
Greider28 <73250074+Greider28@users.noreply.github.com>
2025-04-07 19:48:41 +0300

    Update Solution.java

--- src/com/javarush/test/level13/lesson02/task05/Solution.java
+++ src/com/javarush/test/level13/lesson02/task05/Solution.java
@@ -1,1 +1,64 @@
+package com.javarush.task.task13.task1305;
+
+/* 
+Четыре ошибки
+Исправь 4 ошибки в программе, чтобы она компилировалась.
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        System.out.println(new Dream().HOBBY.toString());
+        System.out.println(new Hobby().toString());
+
+    }
+
+    interface Desire {
+    }
+
+    interface Dream {
+        private static Hobby HOBBY = new Hobby();
+    }
+
+    class Hobby extends Desire implements Dream {
+        static int INDEX = 1;
+
+        @Override
+        public String toString() {
+            INDEX++;
+            return "" + INDEX;
+        }
+    }
+
+}
+/*
+public class Solution {
+
+    public static void main(String[] args) {
+
+        System.out.println(Dream.HOBBY.toString());
+        System.out.println(new Hobby().toString());
+
+    }
+
+    interface Desire {
+    }
+
+    interface Dream {
+        public static Hobby HOBBY = new Hobby();
+    }
+
+    static class Hobby implements Dream, Desire {
+        static int INDEX = 1;
+
+        @Override
+        public String toString() {
+            INDEX++;
+            return "" + INDEX;
+        }
+    }
+
+}
+*/
 
