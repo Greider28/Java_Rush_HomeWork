8aad51dfa799012859b0d3fe954ac233e12af946
Greider28 <73250074+Greider28@users.noreply.github.com>
2025-04-17 20:06:49 +0300

    Update Solution.java

--- src/com/javarush/test/level13/lesson04/task02/Solution.java
+++ src/com/javarush/test/level13/lesson04/task02/Solution.java
@@ -1,1 +1,21 @@
+package com.javarush.task.task13.task1312;
 
+/* 
+Код сам не исправится
+Исправь код так, чтобы программа выполнялась и выводила фразу: "Я переводчик с английского". Метод main менять нельзя!
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        Translator translator = new Translator();
+        System.out.println(translator.translate());
+    }
+
+    public abstract static class Translator {//remove abstract
+        public String translate() {
+            return "Я переводчик с английского";
+        }
+    }
+
+
+}
