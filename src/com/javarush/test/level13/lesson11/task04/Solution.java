e1f0599e11e99bdac4fbbb41e3c663b2f75cdefe
Greider28 <73250074+Greider28@users.noreply.github.com>
2025-04-23 20:33:08 +0300

    Update Solution.java

--- src/com/javarush/test/level13/lesson11/task04/Solution.java
+++ src/com/javarush/test/level13/lesson11/task04/Solution.java
@@ -1,1 +1,34 @@
+package com.javarush.task.task13.task1319;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.InputStreamReader;
+
+/* 
+Писатель в файл с консоли
+В этой задаче тебе нужно: 
+Прочесть с консоли имя файла.
+Считывать строки с консоли, пока пользователь не введет строку "exit".
+Записать абсолютно все введенные в п.2 строки в файл: каждую строчку — с новой строки.
+*/
+
+public class Solution {
+    public static void main(String[] args) {//throws IOException
+        // напишите тут ваш код
+      BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String path = reader.readLine();
+        BufferedWriter writer = new BufferedWriter(new FileWriter(path));
+        String line;
+
+        while (!(line = reader.readLine()).equals("exit")) {// Прерывание цикла при написании строки exit
+            writer.write(line);//пишем строку
+            writer.newLine();//переводим на новую строку в файле
+        }
+        if (line.equals("exit")){
+            writer.write(line);//пишем и последнее слово exit
+        }
+        writer.close();
+    }
+}
 
