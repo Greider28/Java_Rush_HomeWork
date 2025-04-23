24ea77d48bfc6dec72e7602649077e496d1ccada
Greider28 <73250074+Greider28@users.noreply.github.com>
2025-04-23 20:46:52 +0300

    Update Solution.java

--- src/com/javarush/test/level13/lesson11/task06/Solution.java
+++ src/com/javarush/test/level13/lesson11/task06/Solution.java
@@ -1,1 +1,33 @@
+package com.javarush.task.task13.task1321;
 
+
+/* 
+Исправление ошибок
+Переделай наследование в классах и интерфейсах так, чтобы программа компилировалась и продолжала делать то же самое. Класс Hobby должен реализовывать интерфейсы Desire, Dream.
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println(Dream.HOBBY.toString());
+        System.out.println(new Hobby().INDEX);
+    }
+
+    interface Desire {
+    }
+
+    interface Dream implements Hobby {//extends Desire
+        public static Hobby HOBBY = new Hobby();
+    }
+
+    static class Hobby extends Desire, Dream {//implements Desire, Dream
+        static int INDEX = 1;
+
+        @Override
+        public String toString() {
+            INDEX++;
+            return "" + INDEX;
+        }
+    }
+
+}
