78d4c54689d8a831dd849b2ed430f2d0e7e55098
Greider28 <73250074+Greider28@users.noreply.github.com>
2025-04-30 19:42:19 +0300

    Update Solution.java

--- src/com/javarush/test/level13/lesson11/task12/Solution.java
+++ src/com/javarush/test/level13/lesson11/task12/Solution.java
@@ -1,1 +1,142 @@
+package com.javarush.task.task13.task1327;
 
+/*
+Давай напишем программу по мотивам сказки "Репка": 
+Реализуй интерфейс RepkaItem в классе Person.
+В классе Person реализуй метод pull(Person person), который выводит фразу типа '<name> за <person>'. 
+Пример: Бабка за Дедку Дедка за Репку
+Исправь логическую ошибку цикла в методе tell класса RepkaStory.
+Выполни метод main и наслаждайся сказкой!
+*/
+package com.javarush.task.task13.task1327;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Репка
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        List<Person> plot = new ArrayList<Person>();
+        plot.add(new Person("Репка", "Репку"));
+        plot.add(new Person("Дедка", "Дедку"));
+        plot.add(new Person("Бабка", "Бабку"));
+        plot.add(new Person("Внучка", "Внучку"));
+        RepkaStory.tell(plot);
+    }
+}
+
+public class Person {
+    private String name;
+    private String namePadezh;
+
+    public Person(String name, String namePadezh) {
+        this.name = name;
+        this.namePadezh = namePadezh;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNamePadezh() {
+        return namePadezh;
+    }
+
+    public void setNamePadezh(String namePadezh) {
+        this.namePadezh = namePadezh;
+    }
+}
+
+public class RepkaStory {
+    static void tell(List<Person> items) {
+        Person first;
+        Person second;
+        for (int i = items.size() - 1; i > 0; i--) {
+            first = items.get(i - 1);
+            second = items.get(i);
+            first.pull(second);
+        }
+    }
+}
+
+public interface RepkaItem {
+    public String getNamePadezh();
+}
+
+
+/*
+//file Solution.java
+public class Solution {
+    public static void main(String[] args) {
+        List<Person> plot = new ArrayList<Person>();
+        plot.add(new Person("Репка", "Репку"));
+        plot.add(new Person("Дедка", "Дедку"));
+        plot.add(new Person("Бабка", "Бабку"));
+        plot.add(new Person("Внучка", "Внучку"));
+        RepkaStory.tell(plot);
+    }
+}
+
+//file Person.java
+public class Person implements RepkaItem {
+    private String name;
+    private String namePadezh;
+
+    public Person(String name, String namePadezh) {
+        this.name = name;
+        this.namePadezh = namePadezh;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNamePadezh() {
+        return namePadezh;
+    }
+
+    public void setNamePadezh(String namePadezh) {
+        this.namePadezh = namePadezh;
+    }
+
+    public void pull(Person person){
+        System.out.println(this.getName() + " за " + person.getNamePadezh());
+    }
+}
+
+
+//file RepkaItem.java
+public interface RepkaItem {
+    public String getNamePadezh();
+}
+
+
+//file RepkaSory.java
+package com.javarush.task.task13.task1327;
+
+import java.util.List;
+
+public class RepkaStory {
+    static void tell(List<Person> items) {
+        Person first;
+        Person second;
+        for (int i = items.size() - 1; i > 0; i--) {
+            first = items.get(i - 1);
+            second = items.get(i);
+            second.pull(first);
+        }
+    }
+}
+
+*/
