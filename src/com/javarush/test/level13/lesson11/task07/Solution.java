a217026b6054da0a185b5292f8c0e85ed8a74b26
Greider28 <73250074+Greider28@users.noreply.github.com>
2025-04-30 18:22:42 +0300

    Update Solution.java

--- src/com/javarush/test/level13/lesson11/task07/Solution.java
+++ src/com/javarush/test/level13/lesson11/task07/Solution.java
@@ -1,1 +1,24 @@
+package com.javarush.task.task13.task1322;
 
+/* 
+Интерфейс SimpleObject
+Создай класс StringObject и реализуй в нем интерфейс SimpleObject с параметром типа String. Программа должна компилироваться.
+*/
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+        SimpleObject<String> stringObject = new StringObject();
+    }
+
+    interface SimpleObject<T> {
+        SimpleObject<T> getInstance();
+    }
+/*
+  public static class StringObject implements SimpleObject<String> {
+        public SimpleObject<String> getInstance() {
+            return this;
+        }
+    }
+*/
+}
