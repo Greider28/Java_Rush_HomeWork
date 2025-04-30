5926de3c7af8db9d878f6bb076deb9f129650146
Greider28 <73250074+Greider28@users.noreply.github.com>
2025-04-30 18:29:52 +0300

    Update Solution.java

--- src/com/javarush/test/level13/lesson11/task09/Solution.java
+++ src/com/javarush/test/level13/lesson11/task09/Solution.java
@@ -1,1 +1,25 @@
+package com.javarush.task.task13.task1324;
 
+import java.awt.*;
+
+/* 
+Один метод в классе
+Реализуй интерфейс Animal в классе Fox. Поменяй код так, чтобы в классе Fox был только один метод - getName. Учти, что методы удалять нельзя.
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+    }
+
+    public interface Animal {
+        Color getColor();
+
+        Integer getAge();
+    }
+
+    public static class Fox {//public abstract static class Fox implements Animal
+        public String getName() {
+            return "Fox";
+        }
+    }
+}
