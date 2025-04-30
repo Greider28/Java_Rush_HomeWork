c4b1a25725db689adcf13934645433598033a144
Greider28 <73250074+Greider28@users.noreply.github.com>
2025-04-30 19:43:52 +0300

    Update Solution.java

--- src/com/javarush/test/level13/lesson11/task13/Solution.java
+++ src/com/javarush/test/level13/lesson11/task13/Solution.java
@@ -1,1 +1,33 @@
+package com.javarush.task.task13.task1328;
 
+/* 
+Битва роботов
+Для начала разберись в том, что делает код: это будет непросто. Затем измени код согласно новой архитектуре и добавь новую логику: 
+Сделай класс AbstractRobot абстрактным, вынеси логику атаки и защиты из Robot в AbstractRobot.
+Отредактируй класс Robot, учитывая AbstractRobot.
+Расширь класс BodyPart новой частью тела BodyPart.CHEST("грудь").
+Добавь новую часть тела в реализацию интерфейсов Attackable и Defensable (в классе AbstractRobot).
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Robot amigo = new Robot("Амиго");
+        Robot enemy = new Robot("Сгибальщик-02");
+
+        doMove(amigo, enemy);
+        doMove(amigo, enemy);
+        doMove(enemy, amigo);
+        doMove(amigo, enemy);
+        doMove(enemy, amigo);
+        doMove(amigo, enemy);
+        doMove(enemy, amigo);
+        doMove(amigo, enemy);
+    }
+
+    public static void doMove(AbstractRobot robotFirst, AbstractRobot robotSecond) {
+        BodyPart attacked = robotFirst.attack();
+        BodyPart defended = robotSecond.defense();
+        System.out.println(String.format("%s атаковал робота %s, атакована %s, защищена %s",
+                robotFirst.getName(), robotSecond.getName(), attacked, defended));
+    }
+}
