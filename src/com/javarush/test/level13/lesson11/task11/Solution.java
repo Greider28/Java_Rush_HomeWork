0f2ed1c0d588e984286972057cb2097675e1d514
Greider28 <73250074+Greider28@users.noreply.github.com>
2025-04-30 18:37:15 +0300

    Update Solution.java

--- src/com/javarush/test/level13/lesson11/task11/Solution.java
+++ src/com/javarush/test/level13/lesson11/task11/Solution.java
@@ -1,1 +1,38 @@
+package com.javarush.task.task13.task1326;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+/* 
+Сортировка четных чисел из файла
+В этой задаче тебе нужно: 
+Ввести имя файла с консоли.
+Прочитать из него набор чисел.
+Вывести в консоли только четные, отсортированные по возрастанию.
+Пример ввода:
+5
+8
+-2
+11
+3
+-5
+2
+10
+Пример вывода:
+-2
+2
+8
+10
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        // напишите тут ваш код
+
+    }
+}
 
