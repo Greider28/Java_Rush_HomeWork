9dff30724a77201a5a796b060b93f44253feca92
Greider28 <73250074+Greider28@users.noreply.github.com>
2025-05-26 20:45:22 +0300

    Update RussianHen.java

--- src/com/javarush/test/level14/lesson06/task01/RussianHen.java
+++ src/com/javarush/test/level14/lesson06/task01/RussianHen.java
@@ -1,1 +1,13 @@
+package com.javarush.task.task14.task1408;
 
+public class RussianHen extends Hen {
+    @Override
+    public int getCountOfEggsPerMonth() {
+        return 20;
+    }
+
+    public String getDescription(){
+        String result = super.getDescription() + String.format(" Моя страна - %s. Я несу %d яиц в месяц.", Country.RUSSIA, getCountOfEggsPerMonth());
+        return result;
+    }
+}
