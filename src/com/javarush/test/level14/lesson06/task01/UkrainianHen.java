6a0e12d13400f1068f82ae28c82dcd753f30b182
Greider28 <73250074+Greider28@users.noreply.github.com>
2025-05-26 20:44:47 +0300

    Update UkrainianHen.java

--- src/com/javarush/test/level14/lesson06/task01/UkrainianHen.java
+++ src/com/javarush/test/level14/lesson06/task01/UkrainianHen.java
@@ -1,1 +1,13 @@
+package com.javarush.task.task14.task1408;
 
+public class UkrainianHen extends Hen {
+    @Override
+    public int getCountOfEggsPerMonth() {
+        return 50;
+    }
+
+    public String getDescription(){
+        String result = super.getDescription() + String.format(" Моя страна - %s. Я несу %d яиц в месяц.", Country.UKRAINE, getCountOfEggsPerMonth());
+        return result;
+    }
+}
