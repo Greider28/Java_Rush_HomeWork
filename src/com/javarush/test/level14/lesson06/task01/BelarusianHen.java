ba7c50e993417ae342ef00e747725806808e32fe
Greider28 <73250074+Greider28@users.noreply.github.com>
2025-05-26 20:46:45 +0300

    Update BelarusianHen.java

--- src/com/javarush/test/level14/lesson06/task01/BelarusianHen.java
+++ src/com/javarush/test/level14/lesson06/task01/BelarusianHen.java
@@ -1,1 +1,12 @@
+package com.javarush.task.task14.task1408;
 
+public class BelarusianHen extends Hen {
+    public int getCountOfEggsPerMonth() {
+        return 10;
+    }
+
+    public String getDescription(){
+        String result = super.getDescription() + String.format(" Моя страна - %s. Я несу %d яиц в месяц.", Country.BELARUS, getCountOfEggsPerMonth());
+        return result;
+    }
+}
