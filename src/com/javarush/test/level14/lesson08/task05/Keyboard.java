cc4e8ae12834e5fb415ab4c845d1fa4c80b6f058
Greider28 <73250074+Greider28@users.noreply.github.com>
2025-06-16 20:50:33 +0300

    Update Keyboard.java

--- src/com/javarush/test/level14/lesson08/task05/Keyboard.java
+++ src/com/javarush/test/level14/lesson08/task05/Keyboard.java
@@ -1,1 +1,8 @@
+package com.javarush.task.task14.task1413;
 
+public class Keyboard implements CompItem {
+    public String getName(){
+        Class<?> objectClass = this.getClass();
+        return objectClass.getSimpleName(); // Получаем простое имя класса
+    }
+}
