8d4249aa85f8c841ad2a0c5923d940340b1a858b
Greider28 <73250074+Greider28@users.noreply.github.com>
2025-06-19 20:29:31 +0300

    Update Money.java

--- src/com/javarush/test/level14/lesson08/task09/Money.java
+++ src/com/javarush/test/level14/lesson08/task09/Money.java
@@ -1,11 +1,16 @@
 package com.javarush.task.task14.task1417;
 
 public abstract class Money {
+
+    private double amount;//add
+
     public Money(double amount) {
+        this.amount = amount;//add
     }
 
-    public abstract double getAmount();
+    public double getAmount(){//move abstract
+        return this.amount;//add return
+    };
 
     public abstract String getCurrencyName();
 }
-
